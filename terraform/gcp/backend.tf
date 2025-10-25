c2c4c86f7bbc5b466838ea25a1d1eabe4636ba4c
Tisu-r <kim176539@gmail.com>
2025-10-26 02:13:33 +0900

    버킷명안적었네 아오

--- terraform/gcp/backend.tf
+++ terraform/gcp/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "gcs" {
-    bucket  = "your-unique-tfstate-bucket-name" # 미리 만들어 둔 GCS 버킷 이름
+    bucket  = "main-ember-469911-e9-tfstate" # GCS 버킷 이름
     prefix  = "terraform/state"
   }
 }
